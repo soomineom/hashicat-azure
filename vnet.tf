d36356de42305918193fbdbb43e5095bc90914af
soomineom <e5718@naver.com>
2020-11-27 08:01:54 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/soomin-training/network/azurerm"
   version = "3.0.1"
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = "${var.prefix}-vnet"
 }
